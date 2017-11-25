d7678c25dd114a68f0690bafc2e9946d236bbb1f
serzans <serzans@users.noreply.github.com>
2017-11-25 13:31:31 +0000

    getframes

--- getframes.py
+++ getframes.py
@@ -1,11 +1,11 @@
 import cv2
 
-vidcap = cv2.VideoCapture('big_buck_bunny_720p_5mb.mp4')
+vidcap = cv2.VideoCapture('img_4359.mp4')
 success,image = vidcap.read()
 count = 0
 success = True
 while success:
     success,image = vidcap.read()
     print('Read a new frame: ', success)
-    cv2.imwrite("/file/frame%d.jpg" % count, image)     # save frame as JPEG file
+    cv2.imwrite("file/frame%d.jpg" % count, image)     # save frame as JPEG file
     count += 1
